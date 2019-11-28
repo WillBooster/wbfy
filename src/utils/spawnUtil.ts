5ff402123590bb43edcdd586affc4991f4de803b
Sakamoto, Kazunori <exkazuu@gmail.com>
2019-11-28 23:06:05 +0900

    fix: command construction to run external programs

--- src/utils/spawnUtil.ts
+++ src/utils/spawnUtil.ts
@@ -3,7 +3,7 @@ import child_process from 'child_process';
 export function spawnSync(command: string, args: string[], cwd: string): void {
   const commandPrefix =
     process.platform === 'win32' ? '' : `zsh -l -c 'eval "$(nodenv init -)" && nodenv shell "$(nodenv local)" && `;
-  const commandAndArgs = `${commandPrefix}${command} ${args.join(' ')}'`;
+  const commandAndArgs = `${commandPrefix}${command} ${args.join(' ')}`;
   console.log(`$ ${commandAndArgs} at ${cwd}`);
   child_process.spawnSync(commandAndArgs, { cwd, shell: true, stdio: 'inherit' });
 }
