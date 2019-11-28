28cec935786949596bf0ffe56a69b3da9e4c0372
Sakamoto, Kazunori <exkazuu@gmail.com>
2019-11-28 19:27:14 +0900

    feat: support nodenv

--- src/utils/spawnUtil.ts
+++ src/utils/spawnUtil.ts
@@ -1,6 +1,9 @@
 import child_process from 'child_process';
 
 export function spawnSync(command: string, args: string[], cwd: string): void {
-  console.log(`$ ${command} ${args.join(' ')} at ${cwd}`);
-  child_process.spawnSync(command, args, { cwd, shell: true, stdio: 'inherit' });
+  const commandWithNodenv = `zsh -l -c 'eval "$(nodenv init -)" && nodenv shell "$(nodenv local)" && ${command} ${args.join(
+    ' '
+  )}'`;
+  console.log(`$ ${commandWithNodenv} at ${cwd}`);
+  child_process.spawnSync(commandWithNodenv, { cwd, shell: true, stdio: 'inherit' });
 }
