6c32b725c57486ad7de36c1af93417e4249e771e
Sakamoto, Kazunori <exkazuu@gmail.com>
2020-01-29 22:27:40 +0900

    fix: run 'yarn install' when skipping 'yarn add'

--- src/generators/packageJson.ts
+++ src/generators/packageJson.ts
@@ -194,13 +194,19 @@ export async function generatePackageJson(
 
   fse.outputFileSync(filePath, JSON.stringify(jsonObj));
 
+  let yarnInstallRequired = true;
   if (!skipAddingDeps) {
     if (dependencies.length && dependencies.some(dep => !jsonObj.dependencies[dep])) {
       spawnSync('yarn', ['add', '-W', ...new Set(dependencies)], config.dirPath);
+      yarnInstallRequired = false;
     }
     if (devDependencies.length && devDependencies.some(dep => !jsonObj.devDependencies[dep])) {
       spawnSync('yarn', ['add', '-W', '-D', ...new Set(devDependencies)], config.dirPath);
+      yarnInstallRequired = false;
     }
+  }
+  if (yarnInstallRequired) {
+    spawnSync('yarn', ['install'], config.dirPath);
   }
 }
 
