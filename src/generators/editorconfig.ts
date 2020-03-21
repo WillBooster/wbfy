5702678c849cd8830079b7d881e06faf84ce1e4e
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2020-03-21 20:39:39 +0900

    fix: don't add unnecessary {} in .editorconfig (#82)

--- src/generators/editorconfig.ts
+++ src/generators/editorconfig.ts
@@ -11,15 +11,15 @@ end_of_line = lf
 insert_final_newline = true
 trim_trailing_whitespace = true
 
-[*.{${Extensions.codeWith2IndentSize.join(',')}}]
+${generateExtensions(Extensions.codeWith2IndentSize)}
 indent_size = 2
 indent_style = space
 
-[*.{${Extensions.codeWith4IndentSize.join(',')}}]
+${generateExtensions(Extensions.codeWith4IndentSize)}
 indent_size = 4
 indent_style = space
 
-[*.{${Extensions.markdownLike.join(',')}}]
+${generateExtensions(Extensions.markdownLike)}
 max_line_length = off
 trim_trailing_whitespace = false
 
@@ -30,4 +30,8 @@ indent_style = tab
 export async function generateEditorconfig(config: PackageConfig): Promise<void> {
   const filePath = path.resolve(config.dirPath, '.editorconfig');
   await FsUtil.generateFile(filePath, content);
+}
+
+function generateExtensions(extensions: string[]): string {
+  return extensions.length > 1 ? `[*.{${extensions.join(',')}}]` : `[*.${extensions[0]}]`;
 }
