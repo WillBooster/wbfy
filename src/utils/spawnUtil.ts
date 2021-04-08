5b58e2f0cc9ed2fe0342349f7c2b3d7129195fed
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-04-08 23:40:59 +0900

    feat: use asdf instead of nodenv

--- src/utils/spawnUtil.ts
+++ src/utils/spawnUtil.ts
@@ -3,9 +3,13 @@ import child_process from 'child_process';
 export function spawnSync(command: string, args: string[], cwd: string): void {
   let commandAndArgs = `${command} ${args.join(' ')}`;
   if (process.platform !== 'win32') {
-    const version = child_process.execSync('nodenv local || true', { cwd, stdio: 'pipe' }).toString().trim();
+    const [, version] = child_process
+      .execSync('asdf current nodejs || true', { cwd, stdio: 'pipe' })
+      .toString()
+      .split(' ')
+      .filter((s) => !!s);
     if (version && !version.includes(' no ')) {
-      commandAndArgs = `zsh -l -c 'eval "$(nodenv init -)" && nodenv shell "${version}" && ${commandAndArgs}'`;
+      commandAndArgs = `zsh -l -c '. /usr/local/opt/asdf/asdf.sh && ${commandAndArgs}'`;
     }
   }
   console.log(`$ ${commandAndArgs} at ${cwd}`);
