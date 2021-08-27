1d81eb29db6c3089555543be78b4522f644f1c00
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-08-27 19:51:39 +0900

    build: upgrade yarn to 3.0.1

---
+++ __tests__/spawnUtil.ts
@@ -0,0 +1,25 @@
+import child_process from 'child_process';
+import path from 'path';
+
+import { getSpawnSyncArgs } from '../src/utils/spawnUtil';
+
+const testFixturePackageRoot = path.resolve('..', 'test-fixtures-for-wbfy', 'packages');
+
+test.each`
+  dirPath                 | expected
+  ${'yarn1'}              | ${'1.22.10'}
+  ${'yarn1-with-version'} | ${'1.22.10'}
+  ${'yarn2'}              | ${'2.4.2'}
+  ${'yarn2-with-version'} | ${'2.4.2'}
+  ${'yarn3'}              | ${'3.0.1'}
+  ${'yarn3-with-version'} | ${'3.0.1'}
+`('spawnSync on $dirPath repo', ({ dirPath, expected }: { dirPath: string; expected: string }) => {
+  const [commandAndArgs, options] = getSpawnSyncArgs(
+    'yarn',
+    ['--version'],
+    path.resolve(testFixturePackageRoot, dirPath)
+  );
+  options.stdio = 'pipe';
+  const p = child_process.spawnSync(commandAndArgs, options);
+  expect(p.stdout.toString().trim()).toBe(expected);
+});
--- src/utils/spawnUtil.ts
+++ src/utils/spawnUtil.ts
@@ -1,6 +1,12 @@
 import child_process from 'child_process';
 
 export function spawnSync(command: string, args: string[], cwd: string): void {
+  const [commandAndArgs, options] = getSpawnSyncArgs(command, args, cwd);
+  console.log(`$ ${commandAndArgs} at ${options.cwd}`);
+  child_process.spawnSync(commandAndArgs, options);
+}
+
+export function getSpawnSyncArgs(command: string, args: string[], cwd: string): [string, any] {
   const env = { ...process.env };
   // Remove berry from PATH
   if (env.PATH && env.BERRY_BIN_FOLDER) {
@@ -18,6 +24,5 @@ export function spawnSync(command: string, args: string[], cwd: string): void {
       commandAndArgs = `zsh -l -c '. /usr/local/opt/asdf/asdf.sh && ${commandAndArgs}'`;
     }
   }
-  console.log(`$ ${commandAndArgs} at ${cwd}`);
-  child_process.spawnSync(commandAndArgs, { cwd, env, shell: true, stdio: 'inherit' });
+  return [commandAndArgs, { cwd, env, shell: true, stdio: 'inherit' }];
 }
