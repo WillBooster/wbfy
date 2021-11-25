dc3a3e7686c236ba527c7c0519d895c6a2b29573
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-11-25 11:32:05 +0900

    fix: .tool-versions and .node-version support

--- src/generators/asdf.ts
+++ src/generators/asdf.ts
@@ -5,8 +5,10 @@ import { PackageConfig } from '../utils/packageConfig';
 
 export async function generateVersionConfigs(config: PackageConfig): Promise<void> {
   try {
-    const versionsText = await fsp.readFile(path.resolve(config.dirPath, '.tool-versions'), 'utf-8');
+    const toolVersionsPath = path.resolve(config.dirPath, '.tool-versions');
+    const versionsText = await fsp.readFile(toolVersionsPath, 'utf-8');
     const lines = [];
+    const promises = [];
     for (const versionText of versionsText.split('\n')) {
       const line = versionText.trim();
       if (!line.startsWith('nodejs')) {
@@ -15,8 +17,14 @@ export async function generateVersionConfigs(config: PackageConfig): Promise<voi
       }
 
       const [, version] = line.split(/\s+/);
-      await fsp.writeFile(path.resolve(config.dirPath, '.node-version'), version);
+      promises.push(fsp.writeFile(path.resolve(config.dirPath, '.node-version'), version));
     }
+    if (lines.length) {
+      promises.push(fsp.writeFile(toolVersionsPath, lines.join('\n')));
+    } else {
+      promises.push(fsp.rm(toolVersionsPath, { force: true }));
+    }
+    await Promise.all(promises);
   } catch (_) {
     // do nothing
   }
