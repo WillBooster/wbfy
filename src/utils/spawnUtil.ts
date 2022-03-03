38fdf5947a98399c4a638e15a0f717375d741c05
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-03-03 15:05:29 +0900

    feat(gitignore): support Blitz

--- src/utils/spawnUtil.ts
+++ src/utils/spawnUtil.ts
@@ -17,16 +17,22 @@ export function spawnSyncWithStringResult(command: string, args: string[], cwd: 
 }
 
 export function getSpawnSyncArgs(command: string, args: string[], cwd: string): [string, any] {
+  const env = { ...process.env };
+  // Remove berry from PATH
+  if (env.PATH && env.BERRY_BIN_FOLDER) {
+    env.PATH = env.PATH.replace(`${env.BERRY_BIN_FOLDER}:`, '');
+  }
+
   let commandAndArgs = `${command} ${args.join(' ')}`;
-  if (process.env.ASDF_DIR) {
-    commandAndArgs = `bash -l -c '. ${process.env.ASDF_DIR}/asdf.sh && ${commandAndArgs}'`;
+  if (env.ASDF_DIR) {
+    commandAndArgs = `bash -l -c '. ${env.ASDF_DIR}/asdf.sh && ${commandAndArgs}'`;
   }
   return [
     commandAndArgs,
     {
       cwd,
       env: {
-        PATH: process.env.PATH,
+        PATH: env.PATH,
       },
       shell: true,
       stdio: 'inherit',
