72e3c08b4a1706ae577f95f4f6f1e81bd27e8cb8
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-06-08 21:01:55 +0900

    .

--- src/utils/spawnUtil.ts
+++ src/utils/spawnUtil.ts
@@ -2,34 +2,35 @@ import child_process from 'child_process';
 
 export function spawnSync(command: string, args: string[], cwd: string, retry = 0): void {
   do {
-    const [commandAndArgs, options] = getSpawnSyncArgs(command, args, cwd);
-    console.log(`$ ${commandAndArgs} at ${options.cwd}`);
-    const ret = child_process.spawnSync(commandAndArgs, options);
+    const [newCmd, newArgs, options] = getSpawnSyncArgs(command, args, cwd);
+    console.log(`$ ${newCmd} ${newArgs} at ${options.cwd}`);
+    const ret = child_process.spawnSync(newCmd, newArgs, options);
     if (ret.status === 0) break;
   } while (--retry >= 0);
 }
 
 export function spawnSyncWithStringResult(command: string, args: string[], cwd: string): string {
-  const [commandAndArgs, options] = getSpawnSyncArgs(command, args, cwd);
+  const [newCmd, newArgs, options] = getSpawnSyncArgs(command, args, cwd);
   options.stdio = 'pipe';
-  const proc = child_process.spawnSync(commandAndArgs, options);
+  const proc = child_process.spawnSync(newCmd, newArgs, options);
   console.log(proc);
   return proc.stdout.toString().trim();
 }
 
-export function getSpawnSyncArgs(command: string, args: string[], cwd: string): [string, any] {
+export function getSpawnSyncArgs(command: string, args: string[], cwd: string): [string, string[], any] {
   const env = { ...process.env };
   // Remove berry from PATH
   if (env.PATH && env.BERRY_BIN_FOLDER) {
     env.PATH = env.PATH.replace(`${env.BERRY_BIN_FOLDER}:`, '');
   }
 
-  let commandAndArgs = `${command} ${args.join(' ')}`;
   if (env.ASDF_DIR) {
-    commandAndArgs = `bash -l -c '. ${env.ASDF_DIR}/asdf.sh && ${commandAndArgs}'`;
+    args = ['-l', '-c', `. ${env.ASDF_DIR}/asdf.sh && ${command} ${args.join(' ')}`];
+    command = 'bash';
   }
   return [
-    commandAndArgs,
+    command,
+    args,
     {
       cwd,
       env,
