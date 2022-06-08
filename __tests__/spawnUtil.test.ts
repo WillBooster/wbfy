df7c8718d338092cbf8bc71a6f30cc69f57a60e0
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-06-08 21:11:33 +0900

    .

--- __tests__/spawnUtil.test.ts
+++ __tests__/spawnUtil.test.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path from 'path';
 
 import { spawnSyncWithStringResult } from '../src/utils/spawnUtil';
@@ -17,6 +18,8 @@ test.each`
   ${'yarn3-with-tool-versions'} | ${'3.1.1'}
 `('spawnSync on $dirPath repo', ({ dirPath, expected }: { dirPath: string; expected: string }) => {
   const packageDirPath = path.resolve(testFixturePackageRoot, dirPath);
+  console.log(packageDirPath);
+  expect(fs.existsSync(packageDirPath)).toBe(true);
   const version = spawnSyncWithStringResult('yarn', ['--version'], packageDirPath);
   expect(version).toBe(expected);
 });
