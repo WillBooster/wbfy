d89f64ac0f1f661cc612ffacc87c8cc6e0e4d246
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-08-23 19:45:15 +0900

    .

--- src/github/label.ts
+++ src/github/label.ts
@@ -10,29 +10,29 @@ export async function setupLabels(config: PackageConfig): Promise<void> {
     if (!owner || !repo) return;
     if (!config.publicRepo && owner === 'WillBooster') return;
 
-    async function setupLabel(name: string, color: string): Promise<void> {
-      try {
-        await octokit.request('POST /repos/{owner}/{repo}/labels', {
-          owner,
-          repo,
-          name,
-          color,
-        });
-      } catch (e) {
-        await octokit.request('PATCH /repos/{owner}/{repo}/labels/{name}', {
-          owner,
-          repo,
-          name,
-          color,
-        });
-      }
-    }
-
     try {
-      setupLabel('ready', '0E8A16');
-      setupLabel('review requested', 'FBCA04');
+      await setupLabel(owner, repo, 'ready', '0E8A16');
+      await setupLabel(owner, repo, 'review requested', 'FBCA04');
     } catch (e) {
       console.warn('Skip setupLabels due to:', (e as Error)?.stack ?? e);
     }
   });
+}
+
+async function setupLabel(owner: string, repo: string, name: string, color: string): Promise<void> {
+  try {
+    await octokit.request('POST /repos/{owner}/{repo}/labels', {
+      owner,
+      repo,
+      name,
+      color,
+    });
+  } catch (e) {
+    await octokit.request('PATCH /repos/{owner}/{repo}/labels/{name}', {
+      owner,
+      repo,
+      name,
+      color,
+    });
+  }
 }
