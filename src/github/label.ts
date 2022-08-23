78939287aaee6855579d560b0de4aaff5e8dff2d
serpent <pac.ttk0917@gmail.com>
2022-08-23 19:28:58 +0900

    処理を関数化

--- src/github/label.ts
+++ src/github/label.ts
@@ -10,38 +10,27 @@ export async function setupLabels(config: PackageConfig): Promise<void> {
     if (!owner || !repo) return;
     if (!config.publicRepo && owner === 'WillBooster') return;
 
-    try {
+    async function setupLabel(name: string, color: string): Promise<void> {
       try {
         await octokit.request('POST /repos/{owner}/{repo}/labels', {
           owner,
           repo,
-          name: 'ready',
-          color: '0E8A16',
+          name,
+          color,
         });
       } catch (e) {
         await octokit.request('PATCH /repos/{owner}/{repo}/labels/{name}', {
           owner,
           repo,
-          name: 'ready',
-          color: '0E8A16',
+          name,
+          color,
         });
       }
+    }
 
-      try {
-        await octokit.request('POST /repos/{owner}/{repo}/labels', {
-          owner,
-          repo,
-          name: 'review requested',
-          color: 'FBCA04',
-        });
-      } catch (e) {
-        await octokit.request('PATCH /repos/{owner}/{repo}/labels/{name}', {
-          owner,
-          repo,
-          name: 'review requested',
-          color: 'FBCA04',
-        });
-      }
+    try {
+      setupLabel('ready', '0E8A16');
+      setupLabel('review requested', 'FBCA04');
     } catch (e) {
       console.warn('Skip setupLabels due to:', (e as Error)?.stack ?? e);
     }
