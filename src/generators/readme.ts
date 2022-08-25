0ceab92a1ffc912d8ef38ec8ec60f761964fc2d1
suta88 <iwatsuki2828@gmail.com>
2022-08-25 17:42:02 +0900

    readmeのパースおよびsemantic-releaseバッジの挿入

--- src/generators/readme.ts
+++ src/generators/readme.ts
@@ -18,6 +18,19 @@ export async function generateReadme(config: PackageConfig): Promise<void> {
       // が存在するかどうかを確認する。
       // 存在しなければ、 newContent の良い感じの場所に追記する。
       // 良い感じの場所に追記することが少し難しそう。
+      const badgeUrl =
+        '[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)';
+      if (newContent.indexOf(badgeUrl) === -1) {
+        let inserted = false;
+        for (let i = 0; i + 1 < newContent.length; i++) {
+          if (newContent.substring(i, i + 2) === '##') {
+            inserted = true;
+            newContent = newContent.slice(0, i) + badgeUrl + '\n' + newContent.slice(i);
+            break;
+          }
+        }
+        if (!inserted) newContent = newContent + '\n' + badgeUrl + '\n';
+      }
       newContent = newContent + '';
     }
 
