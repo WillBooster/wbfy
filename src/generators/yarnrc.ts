a42c99c28270401af819853591d1b134d2334fe8
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-08-27 09:34:00 +0900

    fix(yarnrc): don't downgrade yarn

--- src/generators/yarnrc.ts
+++ src/generators/yarnrc.ts
@@ -10,50 +10,51 @@ import { spawnSync, spawnSyncWithStringResult } from '../utils/spawnUtil';
 
 export async function generateYarnrcYml(config: PackageConfig): Promise<void> {
   return logger.function('generateYarnrcYml', async () => {
-    await core(config);
-  });
-}
-
-async function core(config: PackageConfig): Promise<void> {
-  const currentVersion = spawnSyncWithStringResult('yarn', ['--version'], config.dirPath);
-  const latestVersion = spawnSyncWithStringResult('npm', ['show', '@yarnpkg/cli', 'version'], config.dirPath);
-  if (currentVersion !== latestVersion) {
-    spawnSync('yarn', ['set', 'version', latestVersion], config.dirPath, 1);
-  }
-
-  const releasesPath = path.join(config.dirPath, '.yarn', 'releases');
-  await fs.promises.mkdir(releasesPath, { recursive: true });
-  for (const file of await fs.promises.readdir(releasesPath)) {
-    if (file.startsWith('yarn-') && !file.startsWith(`yarn-${latestVersion}.`)) {
-      await promisePool.run(() => fs.promises.rm(path.join(releasesPath, file)));
-      console.log('Removed', path.join(releasesPath, file));
+    const currentVersion = spawnSyncWithStringResult('yarn', ['--version'], config.dirPath);
+    const latestVersion = spawnSyncWithStringResult('npm', ['show', '@yarnpkg/cli', 'version'], config.dirPath);
+    if (getMajorNumber(currentVersion) <= getMajorNumber(latestVersion) && currentVersion !== latestVersion) {
+      spawnSync('yarn', ['set', 'version', latestVersion], config.dirPath, 1);
     }
-  }
 
-  const yarnrcPath = path.resolve(config.dirPath, '.yarnrc');
-  await promisePool.run(() => fs.promises.rm(yarnrcPath, { force: true }));
+    const releasesPath = path.join(config.dirPath, '.yarn', 'releases');
+    await fs.promises.mkdir(releasesPath, { recursive: true });
+    for (const file of await fs.promises.readdir(releasesPath)) {
+      if (file.startsWith('yarn-') && !file.startsWith(`yarn-${latestVersion}.`)) {
+        await promisePool.run(() => fs.promises.rm(path.join(releasesPath, file)));
+        console.log('Removed', path.join(releasesPath, file));
+      }
+    }
 
-  const yarnrcYmlPath = path.resolve(config.dirPath, '.yarnrc.yml');
-  const settings = yaml.load(await fs.promises.readFile(yarnrcYmlPath, 'utf8')) as any;
-  settings.defaultSemverRangePrefix = '';
-  if (config.requiringNodeModules) {
-    settings.nodeLinker = 'node-modules';
-    settings.nmMode = 'hardlinks-global';
-  }
-  await fs.promises.writeFile(yarnrcYmlPath, yaml.dump(settings, { lineWidth: -1 }));
+    const yarnrcPath = path.resolve(config.dirPath, '.yarnrc');
+    await promisePool.run(() => fs.promises.rm(yarnrcPath, { force: true }));
 
-  const plugins = (settings.plugins || []).map((p: any) => p.spec as string);
-  const requireTypeScript = config.containingTypeScript || config.containingTypeScriptInPackages;
-  importOrRemovePlugin(config, plugins, requireTypeScript, '@yarnpkg/plugin-typescript');
-  if (requireTypeScript && !config.requiringNodeModules) {
-    spawnSync('yarn', ['dlx', '@yarnpkg/sdks', 'vscode'], config.dirPath);
-  }
-  importOrRemovePlugin(config, plugins, config.containingSubPackageJsons, '@yarnpkg/plugin-workspace-tools');
-  spawnSync('yarn', ['dlx', 'yarn-plugin-auto-install'], config.dirPath);
+    const yarnrcYmlPath = path.resolve(config.dirPath, '.yarnrc.yml');
+    const settings = yaml.load(await fs.promises.readFile(yarnrcYmlPath, 'utf8')) as any;
+    settings.defaultSemverRangePrefix = '';
+    if (config.requiringNodeModules) {
+      settings.nodeLinker = 'node-modules';
+      settings.nmMode = 'hardlinks-global';
+    }
+    await fs.promises.writeFile(yarnrcYmlPath, yaml.dump(settings, { lineWidth: -1 }));
+
+    const plugins = (settings.plugins || []).map((p: any) => p.spec as string);
+    const requireTypeScript = config.containingTypeScript || config.containingTypeScriptInPackages;
+    importOrRemovePlugin(config, plugins, requireTypeScript, '@yarnpkg/plugin-typescript');
+    if (requireTypeScript && !config.requiringNodeModules) {
+      spawnSync('yarn', ['dlx', '@yarnpkg/sdks', 'vscode'], config.dirPath);
+    }
+    importOrRemovePlugin(config, plugins, config.containingSubPackageJsons, '@yarnpkg/plugin-workspace-tools');
+    spawnSync('yarn', ['dlx', 'yarn-plugin-auto-install'], config.dirPath);
+  });
 }
 
 function importOrRemovePlugin(config: PackageConfig, plugins: string[], requirePlugin: boolean, plugin: string): void {
   if (requirePlugin !== plugins.includes(plugin)) {
     spawnSync('yarn', ['plugin', requirePlugin ? 'import' : 'remove', plugin], config.dirPath);
   }
+}
+
+function getMajorNumber(version: string): number {
+  const [major] = version.split('.');
+  return Number(major);
 }
