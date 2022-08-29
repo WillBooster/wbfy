d3b34ae5c33c0164f543ccc6458a09ebe4745482
suta88 <52747166+suta88@users.noreply.github.com>
2022-08-29 13:16:34 +0900

    feat(readme): add semantic-release badge if .releaserc.json exists #300 (#307)
    Co-authored-by: Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>

--- src/generators/readme.ts
+++ src/generators/readme.ts
@@ -6,6 +6,9 @@ import { PackageConfig } from '../packageConfig';
 import { FsUtil } from '../utils/fsUtil';
 import { promisePool } from '../utils/promisePool';
 
+const badgeUrl =
+  '[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)';
+
 export async function generateReadme(config: PackageConfig): Promise<void> {
   return logger.function('generateReadme', async () => {
     const filePath = path.resolve(config.dirPath, 'README.md');
@@ -13,12 +16,20 @@ export async function generateReadme(config: PackageConfig): Promise<void> {
 
     const useSemanticRelease = fs.existsSync(path.resolve(config.dirPath, '.releaserc.json'));
     if (useSemanticRelease) {
-      // TODO: READMEをパースして、
-      // [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
-      // が存在するかどうかを確認する。
-      // 存在しなければ、 newContent の良い感じの場所に追記する。
-      // 良い感じの場所に追記することが少し難しそう。
-      newContent = newContent + '';
+      if (newContent.includes(badgeUrl)) {
+        // 既にbadgeがある場合は削除
+        const badgePos = newContent.indexOf(badgeUrl);
+        newContent = newContent.substring(0, badgePos) + newContent.substring(badgePos + badgeUrl.length);
+      }
+      let inserted = false;
+      for (let i = 0; i < newContent.length; i++) {
+        if (newContent[i] === '\n') {
+          inserted = true;
+          newContent = `${newContent.slice(0, i + 1)}${badgeUrl}\n${newContent.slice(i + 1)}`;
+          break;
+        }
+      }
+      if (!inserted) newContent = `${newContent}\n${badgeUrl}\n`;
     }
 
     await promisePool.run(() => FsUtil.generateFile(filePath, newContent));
