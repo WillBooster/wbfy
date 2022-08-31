0999b7e45c2ab1d0a18c56d13fb3045f2d341fa2
serpent <66120473+serpent0917@users.noreply.github.com>
2022-08-31 16:35:01 +0900

    feat(label): generate more labels (#315)
    Co-authored-by: Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>

--- src/github/label.ts
+++ src/github/label.ts
@@ -11,8 +11,51 @@ export async function setupLabels(config: PackageConfig): Promise<void> {
     if (!config.publicRepo && owner === 'WillBooster') return;
 
     try {
-      await setupLabel(owner, repo, 'ready', '0E8A16');
-      await setupLabel(owner, repo, 'review requested', 'FBCA04');
+      await setupLabel(owner, repo, 'd1: x-easy :hedgehog:', 'EDE9FE');
+      await setupLabel(owner, repo, 'd2: easy :rabbit2:', 'DDD6FE');
+      await setupLabel(owner, repo, 'd3: medium :ox:', 'C4B5FD');
+      await setupLabel(owner, repo, 'd4: hard :squid:', 'A78BFA');
+      await setupLabel(owner, repo, 'd5: x-hard :whale2:', '8B5CF6');
+      await setupLabel(owner, repo, 'p1: critical :fire::fire::fire:', 'EF4444');
+      await setupLabel(owner, repo, 'p2: urgent :fire::fire:', 'F87171');
+      await setupLabel(owner, repo, 'p3: important :fire:', 'FCA5A5');
+      await setupLabel(owner, repo, 'p4: nice to have :droplet:', 'FECACA');
+      await setupLabel(owner, repo, 'r: blitz', '5300bc');
+      await setupLabel(owner, repo, 'r: prisma', '0c344b');
+      await setupLabel(owner, repo, 'r: react', '61dafb');
+      await setupLabel(owner, repo, 'r: svelte', 'ff3e00');
+      await setupLabel(owner, repo, 'ready :rocket:', '22C55E');
+      await setupLabel(owner, repo, 'review requested :mag:', 'FBCA04');
+      await setupLabel(owner, repo, 's: 0.5h :clock1230:', 'F3F4F6');
+      await setupLabel(owner, repo, 's: 1h :clock1:', 'E5E7EB');
+      await setupLabel(owner, repo, 's: 2h :clock2:', 'D1D5DB');
+      await setupLabel(owner, repo, 's: 3h :clock3:', '9CA3AF');
+      await setupLabel(owner, repo, 's: 5h :clock5:', '6B7280');
+      await setupLabel(owner, repo, 's: 8h :clock8:', '4B5563');
+      await setupLabel(owner, repo, 's: 13h :clock1:', '374151');
+      await setupLabel(owner, repo, 't: build :hammer:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: chore :broom:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: ci :construction_worker:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: docs :memo:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: feat :sparkles:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: fix :bug:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: perf :zap:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: refactor :recycle:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: style :lipstick:', 'BFDBFE');
+      await setupLabel(owner, repo, 't: test :test_tube:', 'BFDBFE');
+
+      await deleteLabel(owner, repo, 'bug');
+      await deleteLabel(owner, repo, 'documentation');
+      await deleteLabel(owner, repo, 'duplicate');
+      await deleteLabel(owner, repo, 'enhancement');
+      await deleteLabel(owner, repo, 'good first issue');
+      await deleteLabel(owner, repo, 'help wanted');
+      await deleteLabel(owner, repo, 'invalid');
+      await deleteLabel(owner, repo, 'question');
+      await deleteLabel(owner, repo, 'wontfix');
+      
+      await deleteLabel(owner, repo, 'ready');
+      await deleteLabel(owner, repo, 'review requested');
     } catch (e) {
       console.warn('Skip setupLabels due to:', (e as Error)?.stack ?? e);
     }
@@ -34,5 +77,17 @@ async function setupLabel(owner: string, repo: string, name: string, color: stri
       name,
       color,
     });
+  }
+}
+
+async function deleteLabel(owner: string, repo: string, name: string): Promise<void> {
+  try {
+    await octokit.request('DELETE /repos/{owner}/{repo}/labels/{name}', {
+      owner,
+      repo,
+      name,
+    });
+  } catch (e) {
+    // do nothing
   }
 }
