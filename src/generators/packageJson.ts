4c9ddb53b18b14eb9c5937f7c0314d8f13556aef
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-10-25 22:32:18 +0900

    fix: Blitz v0 support

--- src/generators/packageJson.ts
+++ src/generators/packageJson.ts
@@ -115,7 +115,7 @@ async function core(config: PackageConfig, rootConfig: PackageConfig, skipAdding
     }
   }
 
-  if ((config.containingTypeScript || config.containingTypeScriptInPackages) && config.depending.blitz !== '1') {
+  if (config.containingTypeScript || config.containingTypeScriptInPackages) {
     devDependencies.push('typescript');
   }
 
@@ -205,18 +205,6 @@ async function core(config: PackageConfig, rootConfig: PackageConfig, skipAdding
   }
 
   if (config.depending.blitz) {
-    if (config.depending.blitz === '1') {
-      // These cause an error of eslint-plugin-import loading
-      devDependencies = devDependencies.filter((dep) => !dep.includes('@typescript-eslint/'));
-      // This causes eslint errors
-      devDependencies = devDependencies.filter(
-        (d) =>
-          d !== 'eslint-plugin-react' &&
-          d !== 'eslint-import-resolver-typescript' &&
-          d !== 'eslint-plugin-import' &&
-          d !== 'eslint-plugin-react-hooks'
-      );
-    }
     if (!jsonObj.scripts['gen-code']?.startsWith('blitz codegen')) {
       jsonObj.scripts['gen-code'] = 'blitz codegen';
     }
