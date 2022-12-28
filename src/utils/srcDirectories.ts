2aefb00ee4c2ea6f83300cb8ae6d18035df4c3cd
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-12-28 11:42:25 +0900

    fix: improve recognizer of directory structure

--- src/utils/srcDirectories.ts
+++ src/utils/srcDirectories.ts
@@ -2,10 +2,14 @@ import { PackageConfig } from '../packageConfig';
 
 const srcDirectories = {
   node: ['src', '__tests__', 'scripts'].sort(),
-  // pages は Blitz2 でのみ使用されている。
-  blitz: ['__tests__', 'scripts', 'app', 'db', 'integrations', 'mailers', 'pages', 'test'].sort(),
+  blitz0: ['__tests__', 'scripts', 'app', 'db', 'integrations', 'mailers', 'test'].sort(),
+  // We rename 'test' directory to '__tests__'
+  blitz2: ['src', '__tests__', 'scripts', 'db', 'integrations', 'mailers'].sort(),
 };
 
 export function getSrcDirs(config: PackageConfig): string[] {
-  return config.depending.blitz ? srcDirectories.blitz : srcDirectories.node;
+  if (config.depending.blitz) {
+    return config.depending.blitz === '0' ? srcDirectories.blitz0 : srcDirectories.blitz2;
+  }
+  return srcDirectories.node;
 }
