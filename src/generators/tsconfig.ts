4390a74442a24b68cd39f166d56e408665354d46
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2023-01-01 13:21:54 +0900

    feat: improve esm support (#376)

--- src/generators/tsconfig.ts
+++ src/generators/tsconfig.ts
@@ -3,19 +3,20 @@ import path from 'node:path';
 
 import merge from 'deepmerge';
 import cloneDeep from 'lodash.clonedeep';
+import { TsConfigJson } from 'type-fest';
 
 import { logger } from '../logger';
 import { PackageConfig } from '../packageConfig';
 import { FsUtil } from '../utils/fsUtil';
-import { combineMerge, overwriteMerge } from '../utils/mergeUtil';
+import { combineMerge } from '../utils/mergeUtil';
 import { sortKeys } from '../utils/objectUtil';
 import { promisePool } from '../utils/promisePool';
 
 const rootJsonObj = {
   compilerOptions: {
-    target: 'esnext',
-    module: 'esnext',
-    moduleResolution: 'node',
+    target: 'ESNext',
+    module: 'ESNext',
+    moduleResolution: 'Node',
     jsx: 'react-jsx',
     alwaysStrict: true,
     strict: true,
@@ -41,9 +42,9 @@ const rootJsonObj = {
 
 const subJsonObj = {
   compilerOptions: {
-    target: 'esnext',
-    module: 'esnext',
-    moduleResolution: 'node',
+    target: 'ESNext',
+    module: 'ESNext',
+    moduleResolution: 'Node',
     jsx: 'react-jsx',
     alwaysStrict: true,
     strict: true,
@@ -64,33 +65,38 @@ export async function generateTsconfig(config: PackageConfig, rootConfig: Packag
   return logger.function('generateTsconfig', async () => {
     if (rootConfig.depending.blitz) return;
 
-    let newSettings: any = cloneDeep(config.root ? rootJsonObj : subJsonObj);
+    let newSettings = cloneDeep(config.root ? rootJsonObj : subJsonObj) as TsConfigJson;
     if (!config.containingJsxOrTsx && !config.containingJsxOrTsxInPackages) {
-      delete newSettings.compilerOptions.jsx;
+      delete newSettings.compilerOptions?.jsx;
     }
     if (config.root && !config.containingSubPackageJsons) {
-      newSettings.include = newSettings.include.filter((dirPath: string) => !dirPath.startsWith('packages/*/'));
+      newSettings.include = newSettings.include?.filter((dirPath: string) => !dirPath.startsWith('packages/*/'));
+    }
+    if (config.isEsmPackage) {
+      newSettings.compilerOptions = { ...newSettings.compilerOptions, moduleResolution: 'NodeNext' };
     }
 
     const filePath = path.resolve(config.dirPath, 'tsconfig.json');
     try {
       const existingContent = await fs.promises.readFile(filePath, 'utf8');
-      const oldSettings = JSON.parse(existingContent);
+      const oldSettings = JSON.parse(existingContent) as TsConfigJson;
       if (oldSettings.extends === './node_modules/@willbooster/tsconfig/tsconfig.json') {
         delete oldSettings.extends;
       }
-      delete newSettings?.compilerOptions?.target;
-      delete newSettings?.compilerOptions?.module;
-      if (oldSettings.jsx) {
-        delete newSettings.jsx;
+      // Don't modify "target", "module" and "moduleResolution".
+      delete newSettings.compilerOptions?.target;
+      delete newSettings.compilerOptions?.module;
+      delete newSettings.compilerOptions?.moduleResolution;
+      if (oldSettings.compilerOptions?.jsx) {
+        delete newSettings.compilerOptions?.jsx;
       }
       newSettings = merge.all([newSettings, oldSettings, newSettings], { arrayMerge: combineMerge });
-      newSettings.include = newSettings.include.filter((dirPath: string) => !dirPath.includes('@types'));
+      newSettings.include = newSettings.include?.filter((dirPath: string) => !dirPath.includes('@types'));
     } catch {
       // do nothing
     }
     sortKeys(newSettings.compilerOptions);
-    newSettings.include = newSettings.include.sort();
+    newSettings.include?.sort();
     const newContent = JSON.stringify(newSettings);
     await promisePool.run(() => FsUtil.generateFile(filePath, newContent));
   });
