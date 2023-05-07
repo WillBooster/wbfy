30ff276b5ba5471fdcc4cde4d4468e4ccc63ebe6
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-05-07 15:04:07 +0900

    fix: down grade Next.js from 13.3.1 to 13.3.0

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -1,4 +1,5 @@
 export const PYTHON_VERSION = '3.9.16';
 export const JAVA_VERSION = 'adoptopenjdk-11.0.17+8';
 export const BLITZ_VERSION = '2.0.0-beta.27';
-export const NEXT_VERSION = '13.3.1';
+// Since Next.js 13.3.1 does not work with Blitz.js 2.0.0-beta.27, we use Next.js 13.3.0.
+export const NEXT_VERSION = '13.3.0';
