daf50d6d486a7b4c4ba0b943bd981a59a12ac4ba
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-05-27 09:48:02 +0900

    fix: bump up Next.js from 13.3.0 to 13.3.4

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -1,5 +1,5 @@
 export const PYTHON_VERSION = '3.9.16';
 export const JAVA_VERSION = 'adoptopenjdk-11.0.17+8';
 export const BLITZ_VERSION = '2.0.0-beta.27';
-// Since Next.js 13.3.1 does not work with Blitz.js 2.0.0-beta.27, we use Next.js 13.3.0.
-export const NEXT_VERSION = '13.3.0';
+// Blitz.js 2.0.0-beta.27 works with Next.js 13.3.4.
+export const NEXT_VERSION = '13.3.4';
