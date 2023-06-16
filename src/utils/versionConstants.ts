e7d6616f0569b8c7612e8606ab145ca53667dd6f
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-06-16 09:41:45 +0900

    fix: support the latest Next.js

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -2,4 +2,4 @@ export const PYTHON_VERSION = '3.9.16';
 export const JAVA_VERSION = 'adoptopenjdk-11.0.17+8';
 export const BLITZ_VERSION = '2.0.0-beta.28';
 // Blitz.js 2.0.0-beta.27 works with Next.js 13.3.4.
-export const NEXT_VERSION = '13.4.3';
+export const NEXT_VERSION = '13.4.5';
