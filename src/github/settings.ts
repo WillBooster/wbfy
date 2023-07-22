aa74d8b5a2a4c7eb3f27f48436a48b0af6b7663d
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-07-22 21:11:47 +0900

    .

--- src/github/settings.ts
+++ src/github/settings.ts
@@ -1,3 +1,6 @@
-export async function setupSettings(): Promise<void> {
+import type { PackageConfig } from '../packageConfig.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function setupSettings(config: PackageConfig): Promise<void> {
   // TODO: implement code for https://github.com/WillBooster/wbfy/issues/324
 }
