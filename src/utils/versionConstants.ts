55b8b786127ec7808c67437e7a20f5ba981aded0
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-07-24 17:47:58 +0900

    fix: update version constraints

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -1,4 +1,4 @@
-export const PYTHON_VERSION = '3.9.16';
-export const JAVA_VERSION = 'adoptopenjdk-11.0.17+8';
-export const BLITZ_VERSION = '2.0.0-beta.30';
-export const NEXT_VERSION = '13.4.9';
+export const PYTHON_VERSION = '3.9.17';
+export const JAVA_VERSION = 'zulu-11.66.15';
+export const BLITZ_VERSION = '2.0.0-beta.31';
+export const NEXT_VERSION = '13.4.12';
