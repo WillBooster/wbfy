3f9d6775016e1f8505ad01e43ba361bd9880af69
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-23 16:45:26 +0900

    fix: update next version

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -1,4 +1,4 @@
 export const PYTHON_VERSION = '3.9.17';
 export const JAVA_VERSION = 'zulu-11.66.15';
 export const BLITZ_VERSION = '2.0.0-beta.31';
-export const NEXT_VERSION = '13.4.12';
+export const NEXT_VERSION = '13.4.19';
