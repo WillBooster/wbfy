aab11baa54d6e12e5d0f6fb469edf5f62b3cabfc
toma <tomato@willbooster.com>
2023-09-13 16:08:37 +0900

    fix: blitz version (#484)

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -1,5 +1,5 @@
 export const PYTHON_VERSION = '3.9.17';
 export const JAVA_VERSION = 'zulu-11.66.15';
-export const BLITZ_VERSION = '2.0.0-beta.31';
+export const BLITZ_VERSION = '2.0.0-beta.33';
 // cf. https://github.com/vercel/next.js/issues/53837
 export const NEXT_VERSION = '13.4.12';
