4e3a96f55cecfa00d9778760a5be5382c7a5fe24
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2023-11-01 00:08:34 +0900

    fix: upgrade blitzjs and dependencies (#506)

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -1,4 +1,4 @@
 export const PYTHON_VERSION = '3.9.18';
 export const JAVA_VERSION = 'zulu-11.68.17';
-export const BLITZ_VERSION = '2.0.0-beta.34';
+export const BLITZ_VERSION = '2.0.0-beta.35';
 export const NEXT_VERSION = '13.5.6';
