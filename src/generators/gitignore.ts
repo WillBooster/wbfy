952ddbb62451855e22087eb53b4927d322675c53
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-03-14 13:56:25 +0900

    feat(gitignore): ignore .idea/copilot/

--- src/generators/gitignore.ts
+++ src/generators/gitignore.ts
@@ -12,6 +12,7 @@ import { promisePool } from '../utils/promisePool.js';
 const defaultNames = ['windows', 'macos', 'linux', 'jetbrains', 'visualstudiocode', 'emacs', 'vim', 'yarn'];
 
 const commonContent = `
+.idea/copilot/
 .devcontainer/
 dist/
 temp/
