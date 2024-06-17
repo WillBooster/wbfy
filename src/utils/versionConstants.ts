fef3272343c965dd9758e98645085d19bb91daff
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-06-17 20:14:47 +0900

    fix: update version constraints

--- src/utils/versionConstants.ts
+++ src/utils/versionConstants.ts
@@ -1,4 +1,4 @@
 export const PYTHON_VERSION = '3.9.19';
 export const JAVA_VERSION = 'zulu-11.72.19';
 export const BLITZ_VERSION = '2.0.10';
-export const NEXT_VERSION = '14.2.3';
+export const NEXT_VERSION = '14.2.4';
