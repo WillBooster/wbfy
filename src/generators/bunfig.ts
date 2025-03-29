7f5b324978c858c49f1ca11f5c33b8d972ebf84e
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-03-29 11:29:09 +0900

    fix: improve bunfig generator

--- src/generators/bunfig.ts
+++ src/generators/bunfig.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import path from 'node:path';
 
 import { logger } from '../logger.js';
@@ -5,17 +6,31 @@ import type { PackageConfig } from '../packageConfig.js';
 import { fsUtil } from '../utils/fsUtil.js';
 import { promisePool } from '../utils/promisePool.js';
 
-// Don't add `exact = true` since libraries sometimes want to use non-exact dependencies.
-const newContent = `telemetry = false
+const newContentWithExactTrue = `telemetry = false
+
+[install]
+exact = true
 
 [run]
 bun = true
+`;
 
+const newContentWithExactFalse = `telemetry = false
+
+[install]
+exact = false
+
+[run]
+bun = true
 `;
 
 export async function generateBunfigToml(config: PackageConfig): Promise<void> {
   return logger.functionIgnoringException('generateBunfigToml', async () => {
     const filePath = path.resolve(config.dirPath, 'bunfig.toml');
-    await promisePool.run(() => fsUtil.generateFile(filePath, newContent));
+    const content =
+      fs.existsSync(filePath) && fs.readFileSync(filePath, 'utf8').includes('exact = false')
+        ? newContentWithExactFalse
+        : newContentWithExactTrue;
+    await promisePool.run(() => fsUtil.generateFile(filePath, content));
   });
 }
