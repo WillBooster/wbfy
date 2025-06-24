e78280a5df799bce8e5479fa85f2012c3e26d4af
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-06-24 11:06:50 +0900

    fix: improve biome support

--- src/generators/biome.ts
+++ src/generators/biome.ts
@@ -2,7 +2,6 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 import merge from 'deepmerge';
-import cloneDeep from 'lodash.clonedeep';
 
 import { logger } from '../logger.js';
 import type { PackageConfig } from '../packageConfig.js';
@@ -10,14 +9,13 @@ import { fsUtil } from '../utils/fsUtil.js';
 import { overwriteMerge } from '../utils/mergeUtil.js';
 import { promisePool } from '../utils/promisePool.js';
 
-const jsonObj = {
-  $schema: './node_modules/@biomejs/biome/configuration_schema.json',
-  extends: ['@willbooster/biome-config'],
-};
-
 export async function generateBiomeJsonc(config: PackageConfig): Promise<void> {
   return logger.functionIgnoringException('generateBiomeJsonc', async () => {
-    let newSettings: object = cloneDeep(jsonObj);
+    let newSettings: object = {
+      root: config.isRoot,
+      $schema: './node_modules/@biomejs/biome/configuration_schema.json',
+      extends: ['@willbooster/biome-config'],
+    };
     const filePath = path.resolve(config.dirPath, 'biome.jsonc');
     try {
       const oldContent = await fs.promises.readFile(filePath, 'utf8');
