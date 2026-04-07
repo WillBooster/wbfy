1dc8a4dc90c616f1d77492622cb43dfc7f473241
Sakamoto, Kazunori <exkazuu@gmail.com>
2026-04-07 09:22:56 +0900

    .

--- src/generators/skills.ts
+++ src/generators/skills.ts
@@ -12,19 +12,21 @@ import type { PackageConfig } from '../packageConfig.js';
 
 export async function installAgentSkills(rootConfig: PackageConfig): Promise<void> {
   return logger.functionIgnoringException('installAgentSkills', async () => {
-    // TODO: We are temporally remove agent skills on repo
+    // TODO: We are temporarily removing agent skills from the repo
     // await runInstallAgentSkills(rootConfig);
-    await fs.rm(path.resolve(rootConfig.dirPath, '.agents'), {
-      force: true,
-      recursive: true,
-    });
-    await fs.rm(path.resolve(rootConfig.dirPath, '.claude'), {
-      force: true,
-      recursive: true,
-    });
-    await fs.rm(path.resolve(rootConfig.dirPath, 'skills-lock.json'), {
-      force: true,
-    });
+    await Promise.all([
+      fs.rm(path.resolve(rootConfig.dirPath, '.agents'), {
+        force: true,
+        recursive: true,
+      }),
+      fs.rm(path.resolve(rootConfig.dirPath, '.claude'), {
+        force: true,
+        recursive: true,
+      }),
+      fs.rm(path.resolve(rootConfig.dirPath, 'skills-lock.json'), {
+        force: true,
+      }),
+    ]);
   });
 }
 
